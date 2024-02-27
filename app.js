d54c22be2eb0212c05b5ce7a7d86829dcd3fba9f
James Wimmers <jimmyskydive@gmail.com>
2024-02-28 02:04:56 +1300

    added a basic  card creation of book for now and also added a delte button I need to work out how to make this work properly

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const myLibrary = [];
+const titleEntry = document.getElementById("bookTitle");
+const authorEntry = document.getElementById("bookAuthor");
+const pageNumEntry = document.getElementById("bookPages");
+const buttons = document.querySelectorAll("button");
 
 function Book(title, author, pages) {
   this.title = title;
@@ -10,15 +14,37 @@ function addBookToLibrary(title, author, pages) {
   const bookEntry = new Book(title, author, pages);
   console.log(bookEntry);
   myLibrary.push(bookEntry);
-  console.log(myLibrary);
-  console.log(myLibrary.length);
+  displayBooks(myLibrary);
 }
-const titleEntry = document.getElementById("bookTitle");
-const authorEntry = document.getElementById("bookAuthor");
-const pageNumEntry = document.getElementById("bookPages");
-const buttons = document.querySelectorAll("button");
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+function deleteBook(i) {
+  myLibrary.splice(i, 1);
+  displayBooks(myLibrary);
+}
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     addBookToLibrary(titleEntry.value, authorEntry.value, pageNumEntry.value);
   });
 });
+function displayBooks(books) {
+  let container = document.getElementById("outputContainer");
+  removeAllChildNodes(container);
+  books.forEach((book) => {
+    let newEl = document.createElement("div");
+    newEl.className = "card";
+    newEl.id = "card" + books.indexOf(book);
+    newEl.textContent = book.title + ", " + book.author + ", " + book.pages;
+    let newButton = document.createElement("button");
+    newButton.id = "button" + books.indexOf(book);
+    newButton.textContent = "X";
+    container.appendChild(newEl);
+    container.appendChild(newButton);
+    // newButton.addEventListener("click", deleteBook(books.indexOf(book)));
+  });
+}
+
+addBookToLibrary("Demo", "James", 310);
