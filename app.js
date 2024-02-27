e37756e1d6f53a243c4f385d2afa55f3e82a35d0
James Wimmers <jimmyskydive@gmail.com>
2024-02-27 22:33:34 +1300

    added input fields, titles and a button to html, also added connection to element in html via string in js. Can now log and view new entries and count them with console

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const myLibrary = [];
 
-
-
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
-const titleEntry = "Demo Book";
-const authorEntry = "Myself";
-const pageNumEntry = 256;
 
 function addBookToLibrary(title, author, pages) {
   const bookEntry = new Book(title, author, pages);
   console.log(bookEntry);
   myLibrary.push(bookEntry);
+  console.log(myLibrary);
+  console.log(myLibrary.length);
 }
-
+const titleEntry = document.getElementById("bookTitle");
+const authorEntry = document.getElementById("bookAuthor");
+const pageNumEntry = document.getElementById("bookPages");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    addBookToLibrary(titleEntry, authorEntry, pageNumEntry);
-  });});
+    addBookToLibrary(titleEntry.value, authorEntry.value, pageNumEntry.value);
+  });
+});
